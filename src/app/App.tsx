7577b1c18afe80739e44394fd8be27bdbc86e9be
ovysotskaya <o2.vysotskaya@itransition.com>
2024-01-08 11:48:46 +0300

    add react and configure css in webpack

---
+++ src/app/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.scss';
+
+export const App = () => {
+   return (
+       <div className='app'>
+           Welcome to React and TypeScript
+       </div>
+   )
+}
